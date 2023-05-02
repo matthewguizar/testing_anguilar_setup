212eae7a54eab64d9cfd0ac13d6bfaf99d758167
Matthew Guizar <matthewrguizar@gmail.com>
2023-05-02 16:19:04 -0700

    added bootstrap to table

--- src/app/sales-person-list/sales-person.spec.ts
+++ src/app/sales-person-list/sales-person.spec.ts
@@ -2,6 +2,6 @@ import { SalesPerson } from './sales-person';
 
 describe('SalesPerson', () => {
   it('should create an instance', () => {
-    expect(new SalesPerson()).toBeTruthy();
+    expect(SalesPerson).toBeTruthy();
   });
 });

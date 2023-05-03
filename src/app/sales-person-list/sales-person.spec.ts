3665023c5b0f3bd28e75e0bb1876cef10231bed5
Matthew Guizar <matthewrguizar@gmail.com>
2023-05-02 18:16:38 -0700

    adding conditionals to html

--- src/app/sales-person-list/sales-person.spec.ts
+++ src/app/sales-person-list/sales-person.spec.ts
@@ -2,6 +2,6 @@ import { SalesPerson } from './sales-person';
 
 describe('SalesPerson', () => {
   it('should create an instance', () => {
-    expect(SalesPerson).toBeTruthy();
+    expect(new SalesPerson()).toBeTruthy();
   });
 });
